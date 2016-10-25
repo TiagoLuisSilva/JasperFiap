b6b5ab188ba13927e2194982947e7994b3d1ef75
Alan Henrique <alan_hac@outlook.com>
2016-10-24 23:05:48 -0200

    Front de cliente boleto e notas

--- src/main/resources/static/scripts/app.js
+++ src/main/resources/static/scripts/app.js
@@ -1,15 +1,50 @@
 var app = angular.module('jasperFiap', ["ngRoute"]);
 
 
-app.controller('mainController', [ '$scope', function($scope) {
+app.controller('mainController', function($scope) {
 	$scope.header = {
 		name : 'header',
 		url : 'views/templates/header.html'
 	}
 	
-	$scope.baseURL = 'https://localhost:9000/jasperfiap'
+	$scope.erro = "";
+	
+});
 
-}]);
+
+app.controller('clienteController', function($scope, $http){
+	$http({
+        method : "GET",
+        url : "cliente/consultar"
+    }).then(function mySucces(response) {
+        $scope.clientes = response.data;
+    }, function myError(response) {
+        $scope.erro = response.statusText;
+    });
+});
+
+app.controller('boletoController', function($scope, $http){
+	$http({
+        method : "GET",
+        url : "boleto/consultar"
+    }).then(function mySucces(response) {
+        $scope.boletos = response.data;
+    }, function myError(response) {
+        $scope.erro = response.statusText;
+    });
+});
+
+app.controller('notaController', function($scope, $http){
+	$http({
+        method : "GET",
+        url : "nota/consultar"
+    }).then(function mySucces(response) {
+        $scope.notas = response.data;
+    }, function myError(response) {
+        $scope.erro = response.statusText;
+    });
+});
+
 
 app.config(function($routeProvider) {
     $routeProvider
@@ -17,12 +52,16 @@ app.config(function($routeProvider) {
         templateUrl : "views/templates/home.html"
     })
     .when("/clientes", {
-        templateUrl : "views/clientes.html"
+        templateUrl : "views/clientes.html",
+        controller : "clienteController"
     })
     .when("/boletos", {
-        templateUrl : "views/boletos.html"
+        templateUrl : "views/boletos.html",
+        controller : "boletoController"
     })
     .when("/notas", {
-        templateUrl : "views/notas.html"
+        templateUrl : "views/notas.html",
+        controller : "notaController"
     });
+    
 });
